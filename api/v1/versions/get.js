41a0cb51a63393a0679dc959f19dd43b0993c4a5
Нестор <116899923+Nestor1232323@users.noreply.github.com>
2026-01-07 01:43:07 +0300

    Update get.js

--- api/v1/versions/get.js
+++ api/v1/versions/get.js
@@ -1,1 +1,16 @@
-a
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Use GET method with ?type=latest' });
+  }
+  
+  if (req.query.type !== 'latest') {
+    return res.status(400).json({ error: 'Add ?type=latest to URL' });
+  }
+  
+  res.json({
+    version: 25,
+    name: "Triade OS",
+    description: "<описание потом вставлю>",
+    url: "https://raw.githubusercontent.com/yourusername/yourrepo/main/path/to/file"
+  });
+}
